76e0a81321c884b44ad3b7e3a2b8870e5661448a
文慷黄 <417035863@qq.com>
2018-08-24 17:38:52 +0800

    1.0

---
+++ CodeConfusion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeConfusion
+//
+//  Created by 文慷黄 on 2018/8/24.
+//  Copyright © 2018年 文慷黄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func heolloWorld() {
+        print("123")
+    }
+}
+
---
+++ CodeConfusion/codeObfuscation.h
@@ -0,0 +1,5 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Fri Aug 24 17:36:30 CST 2018
+#define heolloWorld ccFPobfeYrllYnHL
+#endif
